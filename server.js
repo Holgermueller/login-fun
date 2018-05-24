7b3943dd0a6dc4f4379651cc0708277aca4e35f9
Holger Mueller <9jholger@gmail.com>
2018-05-24 11:40:37 -0500

    built server

--- server.js
+++ server.js
@@ -3,30 +3,30 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const exphbs = require('express-handlebars');
 
-//remove later
-const mysql = require('mysql');
-const connection = require('./config/connection.js');
-
 //create port
 const PORT = process.env.PORT || 8080;
 const app = express();
+
+//require models folder
+const db = require("./models");
 
 //use public folder
 app.use(express.static("public"));
 
 //use bodyparser
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //routes
-const routes = require("./controllers/burgers_controllers.js")
-app.use(routes);
+require("./routes/api-routes.js")(app);
 
+//handlebars
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-
 app.set("view engine", "handlebars");
 
 //start server
-app.listen(PORT, function () {
-    console.log("Server listening on: http://localhost: " + PORT);
+db.sequelize.sync().then(function () {
+    app.listen(PORT, function () {
+        console.log("Server listening on: http://localhost: " + PORT);
+    });
 });
