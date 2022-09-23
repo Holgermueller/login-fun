96d8e1ffd8c428b9a5d94219774a08e0cbfad1c3
holgermueller <9jholger@gmail.com>
2022-09-22 22:33:31 -0500

    starting over

--- db/seeds.sql
+++
@@ -1,5 +0,0 @@
-USE sequel_burgers_db;
-INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Juicy Lucy', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', false);
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const exphbs = require("express-handlebars");
-const compression = require("compression");
-const db = require("./models");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(compression());
 app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,11 +11,6 @@ app.use(bodyParser.json());
 
 require("./routes/api-routes.js")(app);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("Server listening on: http://localhost: " + PORT);
-  });
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost: " + PORT);
 });
