3552a4cfa13c12917be0bccc2f765edc4ae95e80
holgermueller <9jholger@gmail.com>
2022-09-22 23:30:22 -0500

    linked pages

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use("/", require("./routes"));
+//app.use("/", require("./routes"));
 app.use(express.static("public"));
 
 app.listen(PORT, () => {
